f73f0cf2866befc74847d8d898d25a24ebbea159
Oq Vinesto Riyadi <38241298+vinesto@users.noreply.github.com>
2018-05-16 16:05:47 +0700

    ganti nama function

--- exercise-2.js
+++ exercise-2.js
@@ -1,11 +1,8 @@
-function balikNama(namaYgMauDibalik){
-  var result='';
-  for(i=1; i<=namaYgMauDibalik.length; i++){
-  result+=namaYgMauDibalik[namaYgMauDibalik.length-i]
-  
+function balikString(namaYgMauDibalik){
+  var result = '';
+  for(var i = 1; i <= namaYgMauDibalik.length; i++){
+  result += namaYgMauDibalik[namaYgMauDibalik.length-i]  
 }
 return result
-  
-  
 }
-console.log(balikNama('test hello'))
+console.log(balikNama('hello world'))
